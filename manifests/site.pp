14baf3698b573c988ae1b423a6727989f10cb537
jimsoccernut <71092824+jimsoccernut@users.noreply.github.com>
2021-05-28 15:33:58 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is a readme stupid',
-    owner   => 'root'
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
